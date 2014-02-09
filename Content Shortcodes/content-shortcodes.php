45f1d877611af04abe4b0afef9f6291036b24109
Daniel Horton <daniel@binaryorganic.com>
2014-02-09 16:02:19 -0500

    initial commit

---
+++ Content Shortcodes/content-shortcodes.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Plugin Name: Content Shortcodes
+ * Plugin URI: http://binaryorganic.com/content-shortcodes
+ * Description: Content Shortcodes creates a custom post type to organize repeatable content that can be embedded into your site using Wordpress shortcodes. Never deal with widgets for repeatable content again.
+ * Version: 0.2
+ * Author: binary/organic
+ * Author URI: http://binaryorganic.com
+ * License: GPL2
+ */
+
+/*  Copyright 2014  binary/organic  (email : contact@binaryorganic.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+
+
+
+/*==========  Create Custom Post Type  ==========*/
+
+
+add_action( 'init', 'content_shortcodes' );
+
+function content_shortcodes() {
+	$labels = array(
+	    'name'               => 'Content Shortcodes',
+	    'singular_name'      => 'Content Shortcode',
+	    'add_new'            => 'Add New',
+	    'add_new_item'       => 'Add New Content Shortcode',
+	    'edit_item'          => 'Edit Content Shortcode',
+	    'new_item'           => 'New Content Shortcode',
+	    'all_items'          => 'All Content Shortcodes',
+	    'view_item'          => 'View Content Shortcode',
+	    'search_items'       => 'Search Content Shortcodes',
+	    'not_found'          => 'No Content Shortcodes found',
+	    'not_found_in_trash' => 'No Content Shortcodes found in Trash',
+	    'parent_item_colon'  => '',
+	    'menu_name'          => 'Shortcodes'
+	  );
+
+	$args = array(
+	    'labels'             => $labels,
+	    'public'             => true,
+	    'publicly_queryable' => false,
+	    'show_ui'            => true,
+	    'show_in_menu'       => true,
+	    'query_var'          => true,
+	    'rewrite'            => array( 'slug' => 'Content Shortcode' ),
+	    'capability_type'    => 'post',
+	    'has_archive'        => true,
+	    'hierarchical'       => false,
+	    'menu_position'      => 5,
+	    'supports'           => array( 'title', 'editor', 'author' ),
+	    'capability_type' => 'post',
+	    'taxonomies' => array('Shortcode Category',),
+	    'not-found' => 'No Categories Found'
+	  );
+
+	register_post_type( 'content_shortcode', $args );
+}
+	
+
+
+
+
+/*==========  Create Category  ==========*/
+
+
+add_action( 'init', 'build_taxonomies', 0 );  
+
+function build_taxonomies() {  
+    register_taxonomy( 'shortcode_cat', 'content_shortcode', array( 
+    	'show_admin_column' => true, 
+    	'hierarchical' => false, 
+    	'label' => 'Categories',
+    	'singuler_name' => 'Category',
+    	'all-items' => 'All Categories',
+    	'edit-item' => 'Edit Category',
+    	'view-item' => 'View Category',
+    	'update-item' => 'Update Cateogry',
+    	'add_new_item' => 'Add new Category',
+    	'new_item_name' => 'New Category',
+    	'menu_name' => 'Categories'
+    	) 
+    );  
+};
+
+
+
+
+
+/*==========  Create Shortcodes  ==========*/
+
+
+add_action( 'init', 'register_content_shortcode');
+
+function register_content_shortcode(){
+   	add_shortcode('content-shortcode', 'content_shortcode');
+}
+
+function content_shortcode($atts, $content = null) {
+    extract( shortcode_atts( array('post_id'  => '1'), $atts ) );
+
+    $post = get_post($post_id); 
+	$content = $post->post_content;
+
+    return $content;
+    
+}
+
+
+
+
+
+/*==========  Custom Columns for Admin List-View  ==========*/
+
+
+add_filter('manage_posts_columns', 'posts_columns_id', 5);
+add_action('manage_posts_custom_column', 'posts_custom_id_columns', 5, 2);
+
+function posts_columns_id($defaults){
+    $defaults['wps_post_id'] = __('ID');
+    return $defaults;
+}
+
+function posts_custom_id_columns($column_name, $id){
+        if($column_name === 'wps_post_id'){
+                echo $id;
+    }
+}
---
+++ Content Shortcodes/readme.txt
@@ -0,0 +1,68 @@
+=== Content Shortcodes ===
+Contributors: binary-organic
+Donate link: http://eff.org
+Tags: content, shortcodes, widgets, repeatable, modular
+Requires at least: 3.0.1
+Tested up to: 3.8.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Content Shortcodes creates a custom post type to organize repeatable content that can be embedded into your site using WordPress shortcodes. Never deal with widgets for repeatable content again.
+
+== Description ==
+
+Content Shortcodes creates a custom post type to organize repeatable content that can be embedded into your site using WordPress shortcodes. Never deal with widgets for repeatable content again.
+
+Using the plugin is incredibly straightforward. You simply create new modular content for your site under the new Shortcodes post type, and then add that content to your site’s posts and pages with a simple shortcode.
+
+Demo Video: https://vimeo.com/85557648
+GitHub URL: https://github.com/binaryorganic/content-shortcodes
+
+
+== Installation ==
+
+There's no setup or customization necessary. Just install the plugin and you're good to go.
+
+There are a few simple steps to getting started with the plugin:
+Create a new Content Shortcode: Click the new Shortcode post-type that has been created in the left-hand menu of WordPress, and then click Add New.
+
+Add some content to your new Content Shortcode. Make sure to give it a title and assign it a category before publishing. Those will only be used to help keep you organized in the admin section and will never be displayed on the front-end of your site, but we recommend using them for quick reference later. When you’re finished, simply click Publish.
+
+Add a Shortcode to your site: Make note of the post-id on the View all Shortcodes screen in the admin section of WordPress. Go into your post or page and add your shortcode using the following format:
+
+[content-shortcode post_id=XX]
+
+where “XX” equals the post-id of the content shortcode you would like to use.
+
+
+== Frequently Asked Questions ==
+
+= What exactly does the plugin do? =
+
+1) Creates a new post-type called Shortcodes
+2) Creates a self-closing shortcode called [content-shortcode]
+3) Adds a column in your View Posts screen that gives you easy access to Post IDs.
+
+= Why not just use widgets? =
+
+Several reasons:
+
+ - It's not really what widgets were intended for
+ - Sites with large amounts of repeatable, modular content get quickly hard to manage using widgets
+ - You get to use the normal WordPress content editor as opposed to that tiny little text box on the widget screen
+ - Your modular content gets its own home in WordPress, which makes site management easy
+
+== Screenshots ==
+
+none
+
+== Changelog ==
+
+= 1.0 =
+* Initial Release
+
+== Upgrade Notice ==
+
+= 1.0 =
+Initial Release
